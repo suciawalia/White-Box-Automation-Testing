8f03c95651f132c4b9126ee1480dde85dfd3583b
suciawalia <suci.awalia35@gmail.com>
2024-04-21 01:34:04 +0700

    update CalculatorTest

--- calculator/src/test/java/com/calculator/CalculatorTest.java
+++ calculator/src/test/java/com/calculator/CalculatorTest.java
@@ -16,10 +16,10 @@ public class CalculatorTest {
     
     @Test
     public void testAddition() {
-        int operand1 = 1;
-        int operand2 = 2;
+        int operand1 = 9;
+        int operand2 = 24;
         char operator = '+';
-        int expectedResult = 3;
+        int expectedResult = 33;
         
         int result = calculator.compute(operand1, operand2, operator);
         
@@ -28,13 +28,39 @@ public class CalculatorTest {
 
     @Test
     public void testSubtraction() {
-        int operand1 = 1;
-        int operand2 = 2;
+        int operand1 = 9;
+        int operand2 = 24;
         char operator = '-';
-        int expectedResult = -1;
+        int expectedResult = -15;
         
         int result = calculator.compute(operand1, operand2, operator);
         
         assertEquals(expectedResult, result);
     }
+
+    @Test
+    public void testMultiplication() {
+        int operand1 = 9;
+        int operand2 = 24;
+        char operator = '*';
+        int expectedResult = 216;
+
+        int result = calculator.compute(operand1, operand2, operator);
+        
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testDivision() {
+        int operand1 = 9;
+        int operand2 = 24;
+        char operator = '/';
+        float expectedResult = 0.375f; // Mengubah tipe data menjadi float
+        
+        float result = calculator.compute(operand1, operand2, operator); // Mengubah tipe data menjadi float
+        
+        assertEquals(expectedResult, result, 0.001f); // Menggunakan delta 0.001 untuk toleransi kesalahan
+    }
+
 }
+
